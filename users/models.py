1c5d1277771ccc7013b511b10cf69d3f6cbc4f41
gutrapp <trappgustavo@gmail.com>
2023-06-28 17:26:49 -0300

    Corrected

--- users/models.py
+++ users/models.py
@@ -4,16 +4,16 @@ class Project(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=200)
 
-class Developer(models.Model):
-    name = models.CharField(max_length=20)
-    projects = models.ManyToManyField(Project)
-
 class Task(models.Model):
     title = models.CharField(max_length=20)
     description = models.CharField(max_length=200)
     complete = models.BooleanField()
-    developers = models.ManyToManyField(Developer)
     project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+class Developer(models.Model):
+    name = models.CharField(max_length=20)
+    projects = models.ManyToManyField(Project, blank=True)
+    tasks = models.ManyToManyField(Task, blank=True)
 
 class Bug(models.Model):
     title = models.CharField(max_length=20)
--- users/serializer.py
+++ users/serializer.py
@@ -1,25 +1,26 @@
 from rest_framework import serializers
 from .models import Project, Developer, Bug, Task
 
-
-class DeveloperSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Developer
-        fields = '__all__'
-
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
 
 class BugSerializer(serializers.ModelSerializer):
-    bugs = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
     class Meta:
         model = Bug
         fields = '__all__'
 
 class TaskSerializer(serializers.ModelSerializer):
+    bugs = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = Task
+        fields = '__all__'
+
+class DeveloperSerializer(serializers.ModelSerializer):
+    projects = ProjectSerializer(many=True, required=False)
+    tasks = TaskSerializer(many=True, required=False)
+
+    class Meta:
+        model = Developer
         fields = '__all__'
